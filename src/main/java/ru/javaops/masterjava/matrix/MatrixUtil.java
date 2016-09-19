b048c40118f8a120823f2024b10ced3897959293
CaymanJava <bosenko_dmitriy@mail.ru>
2016-09-19 22:51:18 +0200

    concurrentMultiply realization

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,6 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
@@ -10,9 +13,42 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
-    // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
-        return null;
+        final int matrixSize = matrixA.length;
+        final int[][] matrixResult = new int[matrixSize][matrixSize];
+        final int threadCount = Runtime.getRuntime().availableProcessors();
+        final int maxIndex = matrixSize * matrixSize;
+        final int cellsInThread =  maxIndex / threadCount;
+        final int[][] matrixBFinal = new int[matrixSize][matrixSize];
+
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                matrixBFinal[i][j] = matrixB[j][i];
+            }
+        }
+
+        Set<Callable<Boolean>> threads = new HashSet<>();
+        int fromIndex = 0;
+        for (int i = 1; i <= threadCount; i++) {
+            final int toIndex = i == threadCount ? maxIndex : fromIndex + cellsInThread;
+            final int firstIndexFinal = fromIndex;
+            threads.add(() -> {
+                for (int j = firstIndexFinal; j < toIndex; j++) {
+                    final int row = j / matrixSize;
+                    final int col = j % matrixSize;
+
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += matrixA[row][k] * matrixBFinal[col][k];
+                    }
+                    matrixResult[row][col] = sum;
+                }
+                return true;
+            });
+            fromIndex = toIndex;
+        }
+        executor.invokeAll(threads);
+        return matrixResult;
     }
 
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
